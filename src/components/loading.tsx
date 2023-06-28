92e9f7a55bb6c97c4c5baaa117f73b88034dcf88
Henry Cortez <henry.cortez@airlinemro.parts>
2023-06-28 11:52:08 -0600

    :bug: Fix navbar

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,8 +1,8 @@
 export const LoadingScreen = () => {
   return (
-    <div className="box">
-      <div className="container">
-        <span className="circle"></span>
+    <div className="loader__box">
+      <div className="loader__container">
+        <span className="loader__circle"></span>
         <span className="circle"></span>
         <span className="circle"></span>
         <span className="circle"></span>
